d2d6827a05b31cd1cdc3a736c7a9717a182092e2
yoavgurevich <ygurevich@ymail.com>
2014-05-12 17:03:09 -0400

    Created Sidebar (Extensions Toolbar) icon and added basic css styling

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 define(function(require, exports, module) {
-  // do something here
+    "use strict";
+
+    var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+
+    var visible = false,
+        realVisibility = false;
+
+    //jQuery objects
+    var $icon;
+
+    //css file import
+    ExtensionUtils.loadStyleSheet(module, "./wvstyle.css");
+
+    function _toggleVisibility() {
+        visible = !visible;
+        _setPanelVisibility(visible);
+    }
+
+    // Add toolbar icon
+    $icon = $("<a class=\"icon ion-headphone\"></a>")
+        .click(_toggleVisibility)
+        .appendTo($("#main-toolbar .buttons"));
 });
