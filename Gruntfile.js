2f34f03eae62aa27a4e2a5f2e05823bf3561c5f8
Kieran Sedgwick <kieran.sedgwick@gmail.com>
2014-05-12 12:55:45 -0400

    Added grunt linting
    Automated linting of our files is a good idea for a distributed development environment. This commit added the necessary packages for the grunt task-runner and configured it for linting our javascript.

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function( grunt ) {
+  // Add the library tasks
+  grunt.loadNpmTasks( "grunt-contrib-jshint" );
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON( "package.json" ),
+    jshint: {
+      files: [
+        "Gruntfile.js",
+        "main.js",
+        "package.json"
+      ]
+    }
+  });
+
+  // Register custom tasks
+  grunt.registerTask( "default", [ "jshint" ]);
+  grunt.registerTask( "travis", [ "jshint" ]);
+};
